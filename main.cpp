a9396b0fa306c338ccbdd2b579c0a05ee17442dc
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-14 21:57:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
+int main()
+{
+
+    char nomi[10][20];
+    char nome[20];
+    int posizione = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            nome[j] = ' ';
+            nomi[i][j] = ' ';
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> nomi[i];
+    }
+    cin >> nome;
+
+    if (find( nomi, nome, posizione))
+    {
+        cout << posizione;
+    }
+    else
+    {
+        cout << "non presente";
+    }
+
+    return 0;
 }
