316e0a045bb29559be6e18c5c38888d396a5a300
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-14 22:01:27 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+//
+// Created by lgian on 14/12/2022.
+//
 
+#ifndef ESERCITAZIONE_9_LIB_H
+#define ESERCITAZIONE_9_LIB_H
+bool find(char lista_nomi[10][20], char target_nome[20], int &posizione);
+#endif //ESERCITAZIONE_9_LIB_H
