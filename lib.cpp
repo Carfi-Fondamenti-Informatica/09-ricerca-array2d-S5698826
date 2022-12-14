402f6b0bb84fe1bc01d999beb6afb6d001a517ac
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-14 22:01:08 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+//
+// Created by lgian on 14/12/2022.
+//
+#include "lib.h"
+bool find(char lista_nomi[10][20], char target_nome[20], int &posizione){
 
+    for (int nome = 0; nome < 10; nome++)
+    {
+        for (int letter = 0; letter < 20; letter++)
+        {
+            if (lista_nomi [nome][letter] != target_nome[letter]){
+                letter = 20;
+            }
+            if (letter == 19){
+                posizione = nome;
+                return true;
+            }
+        }
+    }
+    return false;
+}
